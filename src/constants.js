280d7f4441abc95c5f7911e35c10794e8b843753
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-05-05 03:45:05 -0400

    adds object to constants for thieft player data

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,18 @@
 const CONTRACT_ADDRESS = '0x6fBAf3DE88dac24b38049C6D97Ba50270722d270';
 
-export { CONTRACT_ADDRESS };
+const transformPlayerData = (playerData) => {
+    return {
+        clan: playerData.clan,
+        color: playerData.color,
+        imageURI: playerData.imageURI,
+        class: playerData.class,
+        stealsLeft: playerData.stealsLeft,
+        maxSteals: playerData.maxSteals,
+        totalStealsAttempted: playerData.totalStealsAttempted,
+        daggerCount: playerData.daggerCount,
+        shieldCount: playerData.shieldCount,
+        level: playerData.level
+    };
+  };
+  
+  export { CONTRACT_ADDRESS, transformPlayerData };
