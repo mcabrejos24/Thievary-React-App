0d36538b106bd7f44532fc6c77b506903add3fa0
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-05-12 20:53:36 -0400

    adds logic on stealing

--- src/Components/Arena/index.js
+++ src/Components/Arena/index.js
@@ -8,6 +8,23 @@ const Arena = ({ thiefNFT }) => {
     // State
     const [gameContract, setGameContract] = useState(null);
     const [allPlayers, setAllPlayers] = useState([]);
+
+    const [stealState, setStealState] = useState('');
+
+    const [tokenIdToPlayer, setTokenIdToPlayer] = useState(new Map());
+
+    const stealADagger = async (playerItemId) => {
+      try {
+        setStealState('Successful Stole!');
+
+        // let victimAddress = tokenIdToPlayer.get(playerItemId);
+        // let stealTxn = await gameContract.steal(victimAddress);
+        // await stealTxn.wait();
+      } catch (error) {
+        console.error('Error stealing from a player:', error);
+        setStealState('');
+      }
+    }
   
     // UseEffects
     useEffect(() => {
@@ -37,6 +54,8 @@ const Arena = ({ thiefNFT }) => {
             for(let i=0; i<allPlayersTxn.length; i++) {
                 let playerInfo = await gameContract.getAPlayerNftDetails(allPlayersTxn[i]);
                 playerArr.push(transformPlayerData(playerInfo));
+                // adds to map state of token ids to player addresses
+                setTokenIdToPlayer(prev => new Map([...prev, [playerInfo.itemId.toString(), allPlayersTxn[i].toString()]]));
             }
             setAllPlayers([...playerArr]);
             console.log(allPlayers);
@@ -49,30 +68,46 @@ const Arena = ({ thiefNFT }) => {
 
     }, [gameContract]);
         
+    // function to render all other players that have minted an NFT
     const renderOtherPlayers = () => {
         let finalRender = [];
         if (allPlayers.length > 0) {
-            for (let  player of allPlayers) {
-                finalRender.push(<div className="player">
-                    <div className="image-content">
-                      <h2>{player.color + " " + player.clan}</h2>
-                      <img
-                          src={"https://gateway.pinata.cloud/ipfs/" + player.imageURI.slice(7, player.imageURI.length)}
-                          alt={`Thief ${player.clan}`}
-                      />
-                      <div className="health-bar">
-                          <progress value={player.stealsLeft} max={player.maxSteals} />
-                          <p>{`${player.stealsLeft} / ${player.maxSteals} Steals`}</p>
+            for (let player of allPlayers) {
+              // skip rendering your player
+              if(thiefNFT.itemId.toString() === player.itemId.toString()) continue;
+              finalRender.push(
+                <div className="player-card-container">
+                  <div className={`player-item ${player.color}`} key={player.clan}>
+                    <div className='image-container'>
+                      <img src={"https://gateway.pinata.cloud/ipfs/" + player.imageURI.slice(7, player.imageURI.length)} alt={player.clan} />
+                      <p className="player-class">Class #{player.class.toString()}</p>
+                    </div>
+                    <div className="info-wrapper">
+                        <div className="player-stats">
+                          <progress className="steals-left" value={player.stealsLeft} max={player.maxSteals} />
+                          <p className="steals-left-number">{`${player.stealsLeft} / ${player.maxSteals} Steals`}</p>
+                          <div className="stats">
+                            <p className="total-daggers">{`Total Daggers: ${player.daggerCount}`}</p>
+                            <p className="total-shields">{`Total Shields: ${player.shieldCount}`}</p>
+                          </div>
                         </div>
                     </div>
-                    <div className="stats">
-                      <h4>{`Total Daggers: ${player.daggerCount}`}</h4>
+                    <div className="steal-button-wrapper">
+                      <button
+                        type="button"
+                        className="steal-button"
+                        onClick={()=> stealADagger(player.itemId.toString())}
+                      >Steal A Dagger</button>
                     </div>
-                </div>);
+                  </div>
+                </div>
+              );
             }
           return (
             <div className="players-container">
-              <h2 className="all-players-card-title">Players to steal from:</h2>
+              <div className="all-players-card-title-wrapper">
+                <h2 className="all-players-card-title">Players to steal from:</h2>
+              </div>
               <div className="players-grid">
                 { finalRender }
               </div>
@@ -82,19 +117,21 @@ const Arena = ({ thiefNFT }) => {
       
       }
     }
+
     return (
       <div className="arena-container">
         {thiefNFT && (
-          <div className="your-player-container">
-            <h2 className="player-card-title">Your Player Card:</h2>
+          <div className="player-card-container">
+            <div class="player-card-title-wrapper">
+              <h2 className="player-card-title">Your Player Card: </h2>
+            </div>
             <div className={`player-item ${thiefNFT.color}`} key={thiefNFT.clan}>
               <div className='image-container'>
                 <img src={"https://gateway.pinata.cloud/ipfs/" + thiefNFT.imageURI.slice(7, thiefNFT.imageURI.length)} alt={thiefNFT.clan} />
-                <p class="player-class">Class #{thiefNFT.class.toString()}</p>
+                <p className="player-class">Class #{thiefNFT.class.toString()}</p>
               </div>
               <div className="info-wrapper">
-                <div className="your-player-details">
-                  <div className="your-player-stats">
+                  <div className="player-stats">
                     <progress className="steals-left" value={thiefNFT.stealsLeft} max={thiefNFT.maxSteals} />
                     <p className="steals-left-number">{`${thiefNFT.stealsLeft} / ${thiefNFT.maxSteals} Steals`}</p>
                     <div className="stats">
@@ -102,8 +139,6 @@ const Arena = ({ thiefNFT }) => {
                       <p className="total-shields">{`Total Shields: ${thiefNFT.shieldCount}`}</p>
                     </div>
                   </div>
-                  
-                </div>
               </div>
             </div>
           </div>
